ea763e12721d9d3b4c60f5d0badcab26b240a666
Ari V <ariguitar9@gmail.com>
2024-05-30 17:02:16 -0400

    drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
@@ -13,8 +15,10 @@ public class RobotContainer {
   XboxController driveController = new XboxController(0);
   XboxController peripheralController = new XboxController(1);
   SendableChooser<Command> autoChooser = new SendableChooser<Command>();
+  private final Drivetrain drivetrain = new Drivetrain();
   public RobotContainer() {
     configureBindings();
+    drivetrain.setDefaultCommand(new ArcadeDrive(drivetrain, driveController.getLeftY(), driveController.getRightX()));
   }
 
   private void configureBindings() {}
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,27 +1,35 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 import com.ctre.phoenix6.controls.Follower;
 
-public class Drivetrain {
+public class Drivetrain extends SubsystemBase{
     final TalonFX leftFrontMotor = new TalonFX(DriveConstants.LEFT_FRONT_ID);
     final TalonFX leftRearMotor = new TalonFX(DriveConstants.LEFT_REAR_ID);
     final TalonFX rightFrontMotor = new TalonFX(DriveConstants.RIGHT_FRONT_ID);
     final TalonFX rightRearMotor = new TalonFX(DriveConstants.RIGHT_REAR_ID);
-    final Pigeon2 gyro = new Pigeon2(DriveConstants.GYRO_ID);
-
-    rightRearMotor.setControl(new Follower(DriveConstants.RIGHT_FRONT_ID, false));
-    leftRearMotor.setControl(new Follower(DriveConstants.LEFT_FRONT_ID, false));
 
     final DifferentialDrive drive = new DifferentialDrive(leftFrontMotor::set, rightFrontMotor::set);
+    
     public Drivetrain() {
+        leftRearMotor.setControl(new Follower(DriveConstants.LEFT_FRONT_ID, false));
+        rightRearMotor.setControl(new Follower(DriveConstants.RIGHT_FRONT_ID, false));
     }
     public void ArcadeDrive(Double speed, Double rotation) {
         drive.arcadeDrive(speed, rotation);
     }
+    public void TankDrive(double leftSpeed, double RightSpeed){
+        drive.tankDrive(leftSpeed, RightSpeed);
+    }
+
+    @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+  }
 }
